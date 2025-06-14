31c7a7d5272d07cc6b0380b483651966216e17b5
PetrKornev <kornevpetya@gmail.com>
2025-06-14 19:29:39 +0300

    Functional

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LifecycleComponent from './LifecycleComponent'
+
 
 function App() {
-
-  const [state, setState] = useState(() => 3 + 2);
-  console.log(state);
-  
+  const [counter, setCounter] = useState(1)
+  const incrementForDelete = () => {
+    setCounter((oldCounter) => oldCounter + 1)
+  }
   return <>
+    <h1>Текущее значение : {counter}</h1>
+    {counter % 5 === 0 ? <p>Оп, удален</p> : <LifecycleComponent />}
 
-
-
+    <button onClick={incrementForDelete}>Смэрть</button>
   </>
 }
 
